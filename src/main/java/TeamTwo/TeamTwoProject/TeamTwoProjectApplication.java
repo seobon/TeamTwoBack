bd732c4785e401e8fc653424ad4b8c73fbfeaec8
seobon <seobon1201@naver.com>
2024-02-13 21:12:03 +0900

    Initial commit

---
+++ src/main/java/TeamTwo/TeamTwoProject/TeamTwoProjectApplication.java
@@ -0,0 +1,13 @@
+package TeamTwo.TeamTwoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTwoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTwoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamTwo/TeamTwoProject/TeamTwoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamTwo.TeamTwoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTwoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
